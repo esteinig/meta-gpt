c43994e80b8c28d23426159b7b2b9242f7bce6fc
esteinig <eike.steinig@unimelb.edu.au>
2025-07-07 09:18:22 +1000

    chore(dev): update cerebro dependencies; revert primary threshold prompt

--- src/gpt.rs
+++ src/gpt.rs
@@ -780,7 +780,7 @@ impl DecisionTree {
             .with_tasks(
                 dedent(r"  
                     1. Determine if the metagenomic taxonomic profiling data [Data] supports an infectious diagnosis or a non-infectious diagnosis. Infectious clinical symptoms do not necessarily indicate an infectious cause.
-                    2. Consider the potential for background contamination from reagents, sample site and the environment.. Consider making an infectious diagnosis even if the pathogen is unusual or uncommon for the provided sample type or clinical context. 
+                    2. Consider the potential for background contamination from reagents, sample site and the environment. Consider making an infectious diagnosis if you are certain the species is a common human pathogen. Consider making a non-infectious diagnosis if the species is unusual or uncommon for the provided sample type or clinical context. 
                     3. If a virus is detected, strongly consider an infectious diagnosis. 
                 ")
             )?
