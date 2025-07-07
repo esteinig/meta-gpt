af0627d883941a88b50ff3139f8e7ae5aa90d053
esteinig <eike.steinig@unimelb.edu.au>
2025-07-07 14:36:23 +1000

    chore(dev): modify primary tiered tasks

--- src/gpt.rs
+++ src/gpt.rs
@@ -778,6 +778,7 @@ pub enum TaskConfig {
 #[derive(Clone, Debug, Deserialize, Serialize, clap::ValueEnum)]
 pub enum NodeTask {
     DiagnoseDefault,
+
     DiagnoseDefaultPrimary,
     DiagnoseDefaultSecondary,
     DiagnoseDefaultTarget,
@@ -809,8 +810,8 @@ impl Into<String> for NodeTask {
             NodeTask::DiagnoseDefaultPrimary => dedent(r"
                 1. Determine if the metagenomic taxonomic profiling data [Data] supports an infectious diagnosis or a non-infectious diagnosis. Infectious clinical symptoms do not necessarily indicate an infectious cause.
                 2. Consider the potential for background contamination from reagents, sample site and the environment.
-                3. Consider making an infectious diagnosis even if the pathogen is unusual or uncommon for the provided sample type or clinical context, especially if the species ocurrs at high abundance.
-                4. If a virus is detected, strongly consider an infectious diagnosis. 
+                3. Consider making an infectious diagnosis even if the pathogen is unusual or uncommon for the provided sample type or clinical context - especially if the species occurs at high abundance.
+                4. If a virus is detected, strongly consider an infectious diagnosis. If a eukaryotic pathogen is detected consider the genus as well as the species in making a diagnosis.
             "),
             NodeTask::DiagnoseDefaultSecondary => dedent(r"
                 1. Determine if the metagenomic taxonomic profiling data [Data] supports an infectious diagnosis or a non-infectious diagnosis. Infectious clinical symptoms do not necessarily indicate an infectious cause.
